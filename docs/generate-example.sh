b7ae06ba0cf26ae0770971a1641ff0e7c34a8c3e
demetoir <wnsqlehlswk@naver.com>
2020-10-11 00:39:38 +0900

    docs: add generate-example.sh

---
+++ docs/generate-example.sh
@@ -0,0 +1,6 @@
+yarn generate object-type app/user
+yarn generate input-type app/create-one-user
+yarn generate query-decorator app/get-one-user
+yarn generate mutation-decorator app/create-one-user
+yarn generate interface-type  app/node
+yarn generate enum-type  app/role
