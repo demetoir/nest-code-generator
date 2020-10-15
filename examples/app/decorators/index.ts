58bafff86a8a940cb5f4c040b6b75c0c3c299a68
demetoir <wnsqlehlswk@naver.com>
2020-10-15 21:56:42 +0900

    chore: update example code

--- examples/app/decorators/index.ts
+++ examples/app/decorators/index.ts
@@ -1,3 +1,3 @@
-export * from './create-one-user-input-type-args.decorator'
+export * from './create-one-user-input-args.decorator'
 export * from './get-one-user-query.decorator'
 export * from './create-one-user-mutation.decorator'
